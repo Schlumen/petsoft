c0022f01ca34d1630e389a004ceb6a07b5a6cd00
Schlumen <lumen.haendler@web.de>
2024-10-16 16:40:27 +0200

    finish auth and protect webhook

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,19 +1,40 @@
 import prisma from "@/lib/db";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature");
 
   // verify webhook came from stripe
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature!,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {
+    console.log("Webhook verification failed", error);
+    return Response.json(null, { status: 400 });
+  }
 
-  // fulfil order
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+  switch (event.type) {
+    case "checkout.session.completed":
+      // fulfil order
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email!,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
 
   // return 200 OK response
   return Response.json(null, { status: 200 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -64,11 +64,18 @@ const config = {
         return true;
       }
 
-      if (isLoggedIn && !isTryingToAccessApp) {
+      if (
+        isLoggedIn &&
+        (request.nextUrl.pathname.includes("/login") ||
+          request.nextUrl.pathname.includes("/signup"))
+      ) {
+        return Response.redirect(new URL("/app/dashboard", request.nextUrl));
+      }
+
+      if (isLoggedIn && !isTryingToAccessApp && !auth?.user.hasAccess) {
         if (
-          (request.nextUrl.pathname.includes("/login") ||
-            request.nextUrl.pathname.includes("/signup")) &&
-          !auth?.user.hasAccess
+          request.nextUrl.pathname.includes("/login") ||
+          request.nextUrl.pathname.includes("/signup")
         ) {
           return Response.redirect(new URL("/payment", request.nextUrl));
         }
@@ -82,20 +89,28 @@ const config = {
 
       return false;
     },
-    jwt: ({ token, user }) => {
+    jwt: async ({ token, user, trigger }) => {
       if (user) {
         //on sign in
         token.userId = user.id!;
+        token.email = user.email!;
         token.hasAccess = user.hasAccess;
+      }
+
+      if (trigger === "update") {
+        //on session update
+        const userFromDb = await getUserByEmail(token.email);
+        if (userFromDb) {
+          token.hasAccess = userFromDb.hasAccess;
+        }
       }
 
       return token;
     },
     session: ({ session, token }) => {
-      if (session.user) {
-        session.user.id = token.userId;
-        session.user.hasAccess = token.hasAccess;
-      }
+      session.user.id = token.userId;
+      session.user.hasAccess = token.hasAccess;
+
       return session;
     },
   },
